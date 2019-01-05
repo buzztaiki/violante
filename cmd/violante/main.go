17ad66fe0f27c9e3361d7fc4bf126265013716b1
Taiki Sugawara <buzz.taiki@gmail.com>
2019-01-05 16:11:06 +0900

    split client and server

--- cmd/violante/main.go
+++ cmd/violante/main.go
@@ -3,34 +3,16 @@ package main
 import (
 	"flag"
 	"log"
-	"os"
 
 	"github.com/buzztaiki/violante"
 )
 
 func main() {
-	addr := flag.String("addr", ":51289", "listen address")
-	serverMode := flag.Bool("server", false, "run a server instead of client")
+	addr := flag.String("addr", ":51289", "server listen address")
 	flag.Parse()
 
-	if *serverMode {
-		notifier := violante.NewSlackNotifier(os.Getenv("SLACK_WEBHOOK_URL"), os.Getenv("SLACK_CHANNEL"))
-
-		det, err := violante.NewDetector(os.Getenv("VT_API_KEY"), notifier)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		go det.Start()
-		defer det.Shutdown()
-
-		server := violante.NewServer(*addr, det)
-		if err := server.ListenAndServe(); err != nil {
-			log.Fatal(err)
-		}
-	} else {
-		if err := violante.NewClient(*addr).Add(flag.Args()); err != nil {
-			log.Fatal(err)
-		}
+	client := violante.NewClient(*addr)
+	if err := client.Add(flag.Args()); err != nil {
+		log.Fatal(err)
 	}
 }
